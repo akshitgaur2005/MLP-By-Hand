b85e38e5bff19d096c9ebabf101905c201e99321
akshitgaur2005 <akshitgaur@proton.me>
2024-04-16 10:46:21 +0000

    Created Model Class!

--- trial.py
+++ trial.py
@@ -3,6 +3,12 @@ import numpy as np
 data = np.random.random((4, 3))
 print(data)
 
+def relu(x):
+    if (x >= 0):
+        return x
+    else:
+        return 0
+
 class Dense():
 
     def __init__(self, in_feats, out_feats):
@@ -15,7 +21,22 @@ class Dense():
 dense1 = Dense(3, 2)
 dense2 = Dense(2, 1)
 
-preds1 = dense1.forward(data)
-print(f"preds1: {preds1}")
-preds2 = dense2.forward(preds1)
-print(f"preds2: {preds2}")
+print(f"Pred: {dense2.forward(dense1.forward(data))}")
+
+
+class Model():
+
+    def __init__(self, neurons=[3, 2, 1]):
+        self.layers = []
+        for i in range(len(neurons) - 1):
+            self.layers.append(Dense(neurons[i], neurons[i+1]))
+
+    def forward(self, X):
+        input = X
+        for layer in self.layers:
+            input = layer.forward(input)
+        return input
+
+model = Model()
+
+print(f"Class Pred: {model.forward(data)}")
