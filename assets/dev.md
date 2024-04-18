d5695368e007fac72f380d019f7f805e7ff885c6
akshitgaur2005 <akshitgaur@proton.me>
2024-04-18 06:51:31 +0000

    First Development Diary Released!!

---
+++ assets/dev.md
@@ -0,0 +1,12 @@
+## Development Diaries
+
+I am using this file to basically log the process I use to solve this problem as it has been 2 days and I have not been able to solve it yet.
+
+### 18/04/2024
+
+The forward pass has been completed and was really simple, nothing major to note except for some shape shifting shenanigans.
+
+The part that is still left is backward pass. Here is what I have been able to do till now - 
+
+- Let us have a neural network with 3 layers (excluding the input layer).
+![Base Neural Net](./nn.svg "Neural Network")
--- trial.py
+++ trial.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 #data = np.random.random((1, 1)) # Data shape is samples x features
-data = np.array([[1]])
-labels = np.ones((1, 1))
+data = np.ones((2, 3))
+labels = np.ones((2, 1))
 
 print(f"Data:\n{data}")
 print(f"Labels:\n{labels}")
@@ -16,28 +16,29 @@ def relu_back(x):
 class Dense():
 
     def __init__(self, in_feats, out_feats):
-        self.layer = np.random.random((in_feats, out_feats)) # So that dot product can happen - (samples x features) x (features x neurons in next layer) = (samples x neurons in next layer)
+        self.weights = np.random.random((in_feats, out_feats)) # So that dot product can happen - (samples x features) x (features x neurons in next layer) = (samples x neurons in next layer)
         self.bias = np.random.random((1, out_feats))
         #self.layer = np.array([[1]])
         #self.bias = np.array([[0]])
 
     def forward(self, X):
-        return np.dot(X, self.layer) + self.bias
+        return np.dot(X, self.weights) + self.bias
 
     def backward(self, premult, input):
-        print(f"premult:\n{premult}")
+        #print(f"premult:\n{premult}")
         d_sigma = relu_back(self.forward(input))
-        print(f"d_sigma:\n{d_sigma}")
-        dw = premult * np.multiply(d_sigma, input)
-        print(f"d_sigma * input:\n{np.multiply(d_sigma, input)}")
-        print(f"dw:\n{dw}")
+        print(f"d_sigma: {d_sigma.shape}, input: {input.shape}, weights: {self.weights.shape}")
+        #print(f"d_sigma:\n{d_sigma}")
+        dw = premult * np.multiply(input, d_sigma.T)
+        #print(f"d_sigma * input:\n{np.multiply(d_sigma, input)}")
+        #print(f"dw:\n{dw}")
         db = premult * d_sigma
-        print(f"db:\n{db}")
-        d_last = premult * premult * (d_sigma * self.layer.T).T
-        print(f"d_sigma * self.layer:\n{np.multiply(d_sigma, self.layer.T).T}")
-        print(f"d_last:\n{d_last}")
+        #print(f"db:\n{db}")
+        d_last = premult * premult * (d_sigma * self.weights)
+        #print(f"d_sigma * self.layer:\n{np.multiply(d_sigma, self.weights.T).T}")
+        #print(f"d_last:\n{d_last}")
         return dw, db, d_last
-
+'''
 d1 = Dense(1, 2)
 pred = d1.forward(data)
 print(f"Pred:\n{pred}")
@@ -59,6 +60,7 @@ dw1, db1, dlast1 = d2.backward(premult, pred)
 
 #print(f"dW:\n{dw}\ndb:\n{db}\ndlast:\n{dlast}")
 '''
+
 class Model():
 
     def __init__(self, neurons=[3, 2, 1]):
@@ -68,12 +70,25 @@ class Model():
 
     def forward(self, X):
         input = X
-        for layer in self.layers:
-            input = layer.forward(input)
-            input = relu(input)
-        return input
+        print("-" * 10 + "Forward" + "-" * 10)
+        for i in range(len(self.layers) - 1):
+            print(f"i: {i}, input:\n{input}")
+            input = relu(self.layers[i].forward(input))
+        return self.layers[-1].forward(input)
+
+    def optimise(self, X, y, lr):
+        preds = self.forward(X)
+        premult = 2 * np.mean(relu(preds) - y) # Premult is for chain rule, basically dJ/da_L-1
+
+        for i in range(len(self.layers)):
+            layer = self.layers[len(self.layers) - 1 - i]
+            dw, db, premult = layer.backward(premult, X)
+            layer.weights -= lr * dw
+            layer.bias -= lr * db
+
+        
 
 model = Model([3, 4, 1])
 
 print(f"Class Pred:\n{model.forward(data)}")
-'''
+model.optimise(data, labels, 0.1)
